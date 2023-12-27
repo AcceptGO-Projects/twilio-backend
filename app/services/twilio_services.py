97a4d274aec64df4e8c2fbfe51df4bc5527f64d1
ElJoamy <josephsalguero1@upb.edu>
2023-12-27 16:03:11 -0400

    Refactor Twilio service imports

--- app/services/twilio_services.py
+++ app/services/twilio_services.py
@@ -1,9 +1,9 @@
 from fastapi import HTTPException
-from app.dependencies import get_twilio_client, get_twilio_number
-
 from app.schemas.register_form import RegisterForm
 
 def send_registration_message(form_data: RegisterForm):
+    from app.main import get_twilio_client, get_twilio_number
+
     client = get_twilio_client()
     twilio_number = get_twilio_number()
     try:
