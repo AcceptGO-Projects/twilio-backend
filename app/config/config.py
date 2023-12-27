11319ac1c044971415231bd6329de0ed5055d12c
ElJoamy <josephsalguero1@upb.edu>
2023-12-27 16:03:07 -0400

    Add config file with settings class and get_settings function

---
+++ app/config/config.py
@@ -0,0 +1,18 @@
+from functools import cache
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    service_name: str = "Backend Python FastAPI and Twilio"
+    k_revision: str = "local"
+    log_level: str = "DEBUG"
+    twilio_auth_token: str
+    twilio_account_ssid: str
+    twilio_number: str
+
+    class Config:
+        env_file = ".env"
+
+@cache
+def get_settings():
+    return Settings()
