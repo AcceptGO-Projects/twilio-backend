5ccd091eeb84d24b0f5713afbe9bda13670a37f7
ElJoamy <josephsalguero1@upb.edu>
2023-12-27 16:02:59 -0400

    Add Twilio integration and configuration

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
+import os
+from twilio.rest import Client
+from fastapi import HTTPException
+from app.config.config import get_settings
 from fastapi import FastAPI
 from app.api.router import router as api_router
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+_SETTINGS = get_settings()
 
+app = FastAPI(
+    title=_SETTINGS.service_name,
+    version=_SETTINGS.k_revision
+    )
 
 app.add_middleware(
     CORSMiddleware,
@@ -14,3 +22,24 @@ app.add_middleware(
 )
 
 app.include_router(api_router)
+
+def get_twilio_credentials():
+    account_sid = _SETTINGS.twilio_account_ssid
+    auth_token = _SETTINGS.twilio_auth_token
+    twilio_number = _SETTINGS.twilio_number
+    print(account_sid,auth_token, twilio_number)
+    if not all([account_sid, auth_token, twilio_number]):
+        raise HTTPException(status_code=500, detail="Twilio credentials are not set")
+    return account_sid, auth_token, twilio_number
+
+def get_twilio_client():
+    account_sid, auth_token, _ = get_twilio_credentials()
+    return Client(account_sid, auth_token)
+
+def get_twilio_number() -> str:
+    _, _, twilio_number = get_twilio_credentials()
+    return twilio_number
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("app.main:app", reload=True)
